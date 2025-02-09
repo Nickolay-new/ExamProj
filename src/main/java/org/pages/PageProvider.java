52da2c0c4ca64cede789aa87acc9d5b5824d5129
nikolay.savchenko <nikolay.savchenko@alldigitalads.com>
2025-02-09 10:32:36 +0200

    New directories were added.

---
+++ src/main/java/org/pages/PageProvider.java
@@ -0,0 +1,12 @@
+package org.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageProvider {
+    private WebDriver webDriver;
+
+    public PageProvider(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+}
